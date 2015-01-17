0d445a72293932f69ebf19f7f84c959ffa4367e1
Dario Castañé <i@dario.im>
2015-01-17 19:54:45 +0100

    Importing multiple domain handling from httprouter examples.

---
+++ switch.go
@@ -0,0 +1,24 @@
+// Copyright (c) 2015 Dario Castañé. Licensed under the MIT License.
+// From httprouter's example.
+package medeina
+
+import (
+	"github.com/julienschmidt/httprouter"
+)
+
+// We need an object that implements the http.Handler interface.
+// Therefore we need a type for which we implement the ServeHTTP method.
+// We just use a map here, in which we map host names (with port) to http.Handlers
+type HostSwitch map[string]http.Handler
+
+// Implement the ServerHTTP method on our new type
+func (hs HostSwitch) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Check if a http.Handler is registered for the given host.
+	// If yes, use it to handle the request.
+	if handler := hs[r.Host]; handler != nil {
+		handler.ServeHTTP(w, r)
+	} else {
+		// Handle host names for wich no handler is registered
+		http.Error(w, "Forbidden", 403) // Or Redirect?
+	}
+}
